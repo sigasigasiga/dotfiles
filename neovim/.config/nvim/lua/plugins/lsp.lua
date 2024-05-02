ee23a10414fc4d99dff74e9b513f641a8aad01cc
Egor Bychin <e.siga@proton.me>
2024-05-03 02:41:52 +0400

    neovim: remove upvalues from plugin config

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,69 +1,71 @@
-local ignore_args_wrapper = function(f)
-    return function()
-        f()
-    end
-end
-
-local on_lsp_attach = function(event)
-    local bufnr = event.buf
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    local bufopts = { noremap=true, silent=true }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', 'gl', vim.lsp.buf.references, bufopts)
-
-    vim.api.nvim_create_user_command('DiagList', ignore_args_wrapper(vim.diagnostic.setloclist), {})
-    vim.api.nvim_create_user_command('DiagEnable', ignore_args_wrapper(vim.diagnostic.enable), {})
-    vim.api.nvim_create_user_command('DiagDisable', ignore_args_wrapper(vim.diagnostic.disable), {})
-
-    if client.server_capabilities.documentHighlightProvider then
-        -- number of milliseconds needed for highlight to appear
-        vim.opt.updatetime = 250
-
-        local lsp_highlight_group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
-        vim.api.nvim_clear_autocmds{ buffer = bufnr, group = 'lsp_document_highlight' }
-        vim.api.nvim_create_autocmd('CursorHold', {
-            callback = vim.lsp.buf.document_highlight,
-            buffer = bufnr,
-            group = lsp_highlight_group,
-            desc = 'Document highlight',
-        })
-        vim.api.nvim_create_autocmd('CursorHoldI', {
-            callback = vim.lsp.buf.document_highlight,
-            buffer = bufnr,
-            group = lsp_highlight_group,
-            desc = 'Document highlight',
-        })
-        vim.api.nvim_create_autocmd('CursorMoved', {
-            callback = vim.lsp.buf.clear_references,
-            buffer = bufnr,
-            group = lsp_highlight_group,
-            desc = 'Clear all the references',
-        })
-    end
-
-    if client.server_capabilities.documentFormattingProvider then
-        local lsp_format_group = vim.api.nvim_create_augroup('lsp_format', { clear = false })
-        vim.api.nvim_clear_autocmds{ buffer = bufnr, group = 'lsp_format' }
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            -- I don't know why is it needed to wrap the callback but it is what it is
-            callback = ignore_args_wrapper(vim.lsp.buf.format),
-            buffer = bufnr,
-            group = lsp_fromat_group,
-            desc = 'Format document on write'
-        })
-    end
-end
-
 return function()
+    -- unfortunately, `packer.nvim` doesn't support upvalues in config functions
+    -- TODO: try `lazy.nvim`
+    local ignore_args_wrapper = function(f)
+        return function()
+            f()
+        end
+    end
+
+    local on_lsp_attach = function(event)
+        local bufnr = event.buf
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        local bufopts = { noremap=true, silent=true }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', 'gl', vim.lsp.buf.references, bufopts)
+
+        vim.api.nvim_create_user_command('DiagList', ignore_args_wrapper(vim.diagnostic.setloclist), {})
+        vim.api.nvim_create_user_command('DiagEnable', ignore_args_wrapper(vim.diagnostic.enable), {})
+        vim.api.nvim_create_user_command('DiagDisable', ignore_args_wrapper(vim.diagnostic.disable), {})
+
+        if client.server_capabilities.documentHighlightProvider then
+            -- number of milliseconds needed for highlight to appear
+            vim.opt.updatetime = 250
+
+            local lsp_highlight_group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
+            vim.api.nvim_clear_autocmds{ buffer = bufnr, group = 'lsp_document_highlight' }
+            vim.api.nvim_create_autocmd('CursorHold', {
+                callback = vim.lsp.buf.document_highlight,
+                buffer = bufnr,
+                group = lsp_highlight_group,
+                desc = 'Document highlight',
+            })
+            vim.api.nvim_create_autocmd('CursorHoldI', {
+                callback = vim.lsp.buf.document_highlight,
+                buffer = bufnr,
+                group = lsp_highlight_group,
+                desc = 'Document highlight',
+            })
+            vim.api.nvim_create_autocmd('CursorMoved', {
+                callback = vim.lsp.buf.clear_references,
+                buffer = bufnr,
+                group = lsp_highlight_group,
+                desc = 'Clear all the references',
+            })
+        end
+
+        if client.server_capabilities.documentFormattingProvider then
+            local lsp_format_group = vim.api.nvim_create_augroup('lsp_format', { clear = false })
+            vim.api.nvim_clear_autocmds{ buffer = bufnr, group = 'lsp_format' }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                -- I don't know why is it needed to wrap the callback but it is what it is
+                callback = ignore_args_wrapper(vim.lsp.buf.format),
+                buffer = bufnr,
+                group = lsp_fromat_group,
+                desc = 'Format document on write'
+            })
+        end
+    end
+
     vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp_user_config', {}),
-        callback = function(ev) on_lsp_attach(ev) end -- i don't know why but it needs to be wrapped
+        callback = on_lsp_attach
     })
 
     lspconfig = require('lspconfig')
