dcfa15cdc58262d9bb4e2940ec5a4386b93cfdbf
Egor Bychin <e.siga@proton.me>
2024-05-08 16:36:33 +0400

    neovim: use nvim-telescope

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -65,6 +65,16 @@ return require('packer').startup(function(use)
     }
 
     use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        config = function()
+            local bufopts = { noremap=true, silent=true }
+            local telescope = require('telescope.builtin')
+            vim.keymap.set('n', '\\fg', telescope.git_files, bufopts)
+        end
+    }
+
+    use {
         'nvim-tree/nvim-tree.lua',
         config = require('plugin-config.nvim-tree')
     }
