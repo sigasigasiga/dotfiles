211e1e467872f13bc798f9dec2a6c9c2e2fa7199
Egor Bychin <e.siga@proton.me>
2024-11-07 13:22:21 +0400

    neovim: lsp: setup servers conditionally

--- neovim/.config/nvim/lua/lazy-config/lsp.lua
+++ neovim/.config/nvim/lua/lazy-config/lsp.lua
@@ -1,77 +1,85 @@
-local cfg = function()
-    local on_lsp_attach = function(event)
-        local bufnr = event.buf
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        assert(client)
+local setup_language_server = function(server_name, params)
+    local lspconfig = require'lspconfig'
+    local server_exec = (params.cmd or lspconfig[server_name].config_def.default_config.cmd)[1]
+    if vim.fn.executable(server_exec) == 1 then
+        lspconfig[server_name].setup(params)
+    end
+end
 
-        -- Enable completion triggered by <C-x><C-o>
-        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+local on_lsp_attach = function(event)
+    local bufnr = event.buf
 
-        -- 'c' stands for 'code'
-        local bufopts = { noremap = true, silent = true }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', '<Leader>ci', vim.lsp.buf.implementation, bufopts) -- 'i' -> implementation
-        vim.keymap.set('n', '<Leader>cl', vim.lsp.buf.references, bufopts) -- 'l' -> list
-        vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set({'n', 'i'}, '<C-s>', vim.lsp.buf.signature_help, bufopts) -- 's' -> signature
+    -- Enable completion triggered by <C-x><C-o>
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-        if client.server_capabilities.documentHighlightProvider then
-            -- number of milliseconds needed for highlight to appear
-            vim.opt.updatetime = 250
+    -- 'c' stands for 'code'
+    local bufopts = { noremap = true, silent = true }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', '<Leader>ci', vim.lsp.buf.implementation, bufopts) -- 'i' -> implementation
+    vim.keymap.set('n', '<Leader>cl', vim.lsp.buf.references, bufopts) -- 'l' -> list
+    vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set({'n', 'i'}, '<C-s>', vim.lsp.buf.signature_help, bufopts) -- 's' -> signature
 
-            local lsp_highlight_group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
-            vim.api.nvim_clear_autocmds { buffer = bufnr, group = 'lsp_document_highlight' }
-            vim.api.nvim_create_autocmd('CursorHold', {
-                callback = vim.lsp.buf.document_highlight,
-                buffer = bufnr,
-                group = lsp_highlight_group,
-                desc = 'Document highlight',
-            })
-            vim.api.nvim_create_autocmd('CursorHoldI', {
-                callback = vim.lsp.buf.document_highlight,
-                buffer = bufnr,
-                group = lsp_highlight_group,
-                desc = 'Document highlight',
-            })
-            vim.api.nvim_create_autocmd('CursorMoved', {
-                callback = vim.lsp.buf.clear_references,
-                buffer = bufnr,
-                group = lsp_highlight_group,
-                desc = 'Clear all the references',
-            })
-        end
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    assert(client)
+    if client.server_capabilities.documentHighlightProvider then
+        -- number of milliseconds needed for highlight to appear
+        vim.opt.updatetime = 250
 
-        if client.server_capabilities.documentFormattingProvider then
-            local lsp_format_group = vim.api.nvim_create_augroup('lsp_format', { clear = false })
-            vim.api.nvim_clear_autocmds { buffer = bufnr, group = 'lsp_format' }
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                -- I don't know why is it needed to wrap the callback but it is what it is
-                callback = function() vim.lsp.buf.format() end,
-                buffer = bufnr,
-                group = lsp_format_group,
-                desc = 'Format document on write'
-            })
-        end
+        local lsp_highlight_group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
+        vim.api.nvim_clear_autocmds { buffer = bufnr, group = 'lsp_document_highlight' }
+        vim.api.nvim_create_autocmd('CursorHold', {
+            callback = vim.lsp.buf.document_highlight,
+            buffer = bufnr,
+            group = lsp_highlight_group,
+            desc = 'Document highlight',
+        })
+        vim.api.nvim_create_autocmd('CursorHoldI', {
+            callback = vim.lsp.buf.document_highlight,
+            buffer = bufnr,
+            group = lsp_highlight_group,
+            desc = 'Document highlight',
+        })
+        vim.api.nvim_create_autocmd('CursorMoved', {
+            callback = vim.lsp.buf.clear_references,
+            buffer = bufnr,
+            group = lsp_highlight_group,
+            desc = 'Clear all the references',
+        })
     end
 
+    if client.server_capabilities.documentFormattingProvider then
+        local lsp_format_group = vim.api.nvim_create_augroup('lsp_format', { clear = false })
+        vim.api.nvim_clear_autocmds { buffer = bufnr, group = 'lsp_format' }
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            -- I don't know why is it needed to wrap the callback but it is what it is
+            callback = function() vim.lsp.buf.format() end,
+            buffer = bufnr,
+            group = lsp_format_group,
+            desc = 'Format document on write'
+        })
+    end
+end
+
+
+local cfg = function()
     vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp_user_config', {}),
         callback = on_lsp_attach
     })
 
-    local lspconfig = require('lspconfig')
+    setup_language_server('pyright', {})
+    setup_language_server('rust_analyzer', {})
 
-    lspconfig.clangd.setup {
+    setup_language_server('clangd', {
         cmd = { 'clangd', '--fallback-style=none' },
         on_attach = function(client, bufnr)
             vim.keymap.set('n', '<Leader>cs', ':ClangdSwitchSourceHeader<CR>', bufopts) -- 's' -> switch
         end
-    }
+    })
 
-    lspconfig.pyright.setup {}
-    lspconfig.rust_analyzer.setup {}
-    lspconfig.lua_ls.setup {
+    setup_language_server('lua_ls', {
         on_init = function(client)
             local path = client.workspace_folders[1].name
             if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
@@ -108,7 +116,7 @@ local cfg = function()
                 }
             }
         }
-    }
+    })
 end
 
 return {
