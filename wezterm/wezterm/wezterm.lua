7f203519de65f30374f4d3c01c88ab4a0529cfbc
Egor Bychin <e.siga@proton.me>
2024-12-05 22:34:01 +0400

    wezterm: remove windows config

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -1,12 +1,6 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    require('windows').fill_config(config)
-end
-
--- currently it is required to call `common` after the platform-specific configs,
--- because we need to know if `config.launch_menu` is filled or not
 require('common').fill_config(config)
 
 return config
