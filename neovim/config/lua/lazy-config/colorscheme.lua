115347f4f5388c6d53154de32e50322aac0846cf
Egor Bychin <e.siga@proton.me>
2025-04-15 19:00:50 +0400

    neovim: gruvbox: use hard contrast

--- neovim/config/lua/lazy-config/colorscheme.lua
+++ neovim/config/lua/lazy-config/colorscheme.lua
@@ -2,6 +2,7 @@ local make_gruvbox_opts = function()
     local gruvbox_gray = '#504945'
 
     return {
+        contrast = 'hard',
         overrides = {
             LspReferenceRead = { bg = gruvbox_gray },
             LspReferenceText = { bg = gruvbox_gray },
