22ab9a4d0f0f39b07021f84a8590382775d98c44
Egor Bychin <e.siga@proton.me>
2025-04-23 01:53:51 +0400

    neovim: use CSI 2031

--- neovim/config/lua/plugins.lua
+++ neovim/config/lua/plugins.lua
@@ -9,4 +9,21 @@ require('lazy').setup{
     local_spec = false,
 }
 
-require('rplugin-config.dark-monitor')
+-- TODO move it somewhere else?
+local set_colorscheme = function()
+    local bg = vim.opt.background:get()
+    if bg == 'dark' then
+        vim.cmd.colorscheme 'gruvbox'
+    elseif bg == 'light' then
+        vim.cmd.colorscheme 'catppuccin-latte'
+    else
+        assert(false, 'wtf is your background')
+    end
+end
+
+vim.api.nvim_create_autocmd('OptionSet', {
+    pattern = 'background',
+    callback = function(ev)
+        set_colorscheme()
+    end
+})
