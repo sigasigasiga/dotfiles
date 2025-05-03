47e75ee25c22d0a13aa727d0dfd74fbef6e3b20d
Egor Bychin <e.siga@proton.me>
2025-05-04 00:09:48 +0400

    neovim: add `:SetCodestyle` command

--- neovim/config/lua/codestyle.lua
+++ neovim/config/lua/codestyle.lua
@@ -1,31 +1,22 @@
-local drw_codestyle = {
-    -- tab settings (expandtab changes tabs to spaces)
-    tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true,
-    -- autoindent settings. cinoptions are fucky, so tldr:
-    -- `l1` = don't fuck up curly braces in `case`
-    -- `g0` = access modifiers are not indented
-    -- `N-s` = namespaces are not indented
-    -- `(0,W4` = `void foo(<CR> /* next line is 4 space indented`
-    -- `(s,m1` = `void foo(<CR>` will have `)` on beginning of the next line
-    -- `j1` = don't fuck up lambda definitions in an argument list
-    -- `J1` = don't fuck up JS (and cpp2!) object declarations
-    ai = true, cin = true, cinoptions = 'l1,g0,N-s,(0,W4,(s,m1,j1,J1'
-}
+local codestyles = {
+    drw = {
+        -- tab settings (expandtab changes tabs to spaces)
+        tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true,
+        -- autoindent settings. cinoptions are fucky, so tldr:
+        -- `l1` = don't fuck up curly braces in `case`
+        -- `g0` = access modifiers are not indented
+        -- `N-s` = namespaces are not indented
+        -- `(0,W4` = `void foo(<CR> /* next line is 4 space indented`
+        -- `(s,m1` = `void foo(<CR>` will have `)` on beginning of the next line
+        -- `j1` = don't fuck up lambda definitions in an argument list
+        -- `J1` = don't fuck up JS (and cpp2!) object declarations
+        ai = true, cin = true, cinoptions = 'l1,g0,N-s,(0,W4,(s,m1,j1,J1',
+    },
 
-local gnu_codestyle = {
-    tabstop = 8, shiftwidth = 2, softtabstop = 2, expandtab = false,
-    ai = true, cin = true, cinoptions='>4,n-2,{2,^-2,:2,=2,g0,h2,p5,t0,+2,(0,u0,w1,m1'
-}
-
-local glidewell_codestyle = {
-    tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true,
-    -- `:0` = place `case`s with the same indent as `switch`
-    -- `g0` = access modifiers are not indented
-    -- `N-s` = namespaces are not indented
-    -- `(0` = table-style argument formatting
-    -- `j1` = don't fuck up lambda definitions in an argument list
-    -- `J1` = don't fuck up JS (and cpp2!) object declarations
-    ai = true, cin = true, cinoptions = ':0,g0,N-s,(0,j1,J1'
+    gnu = {
+        tabstop = 8, shiftwidth = 2, softtabstop = 2, expandtab = false,
+        ai = true, cin = true, cinoptions='>4,n-2,{2,^-2,:2,=2,g0,h2,p5,t0,+2,(0,u0,w1,m1',
+    }
 }
 
 local set_codestyle = function(style)
@@ -35,10 +26,17 @@ local set_codestyle = function(style)
 end
 
 -- set default codestyle
-set_codestyle(drw_codestyle)
+set_codestyle(codestyles.drw)
 
--- TODO: reset the codestyle when opening another file after opening a glidewell file (idk how tho)
-local id = vim.api.nvim_create_augroup('ProjectSetup', {})
+-- set codestyle with ease
+vim.api.nvim_create_user_command(
+    'SetCodestyle',
+    function(params) set_codestyle(codestyles[params.args]) end,
+    { nargs = 1 }
+)
+
+-- TODO: change to RC codestyle
+local id = vim.api.nvim_create_augroup('ProjectSetup', {}) -- TODO: start all augroups with `siga/`
 vim.api.nvim_create_autocmd({'BufRead', 'BufEnter'}, {
     pattern = 'C:/projects/glidewell/*',
     group = id,
@@ -46,5 +44,3 @@ vim.api.nvim_create_autocmd({'BufRead', 'BufEnter'}, {
         set_codestyle(glidewell_codestyle)
     end
 })
-
--- TODO: FIXME: create functions to easily switch between codestyles
