1d734dddb91dad0e31052729513e5686fb2fef53
Egor Bychin <e.siga@proton.me>
2026-02-20 18:23:06 +0400

    neovim: keymaps: use `vim.keymap.set`

--- neovim/config/lua/keymaps.lua
+++ neovim/config/lua/keymaps.lua
@@ -1,17 +1,20 @@
--- MARK: shortcuts
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-local normal_mode = 'n'
-
--- MARK: actual keymap configs
-
--- copy `<filename>:<line_number>`
--- FIXME: remove duplicate keymaps
-keymap(normal_mode, 'yp', ':let @+=expand("%:.") . ":" . line(".")<CR>', opts)
-keymap(normal_mode, 'yP', ':let @+=expand("%:.") . ":" . line(".")<CR>', opts)
-
--- unmap the default behavior of `gr` so that it wouldn't clash with the default lsp mappings
-keymap(normal_mode, 'gr', '', opts)
-
+-- leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+local opts = { noremap = true, silent = true }
+
+-- copy `<filename>:<line_number>`
+vim.keymap.set(
+    'n',
+    '<Leader>yp',
+    function()
+        local path = vim.fn.expand('%:.') -- Get _relative_ path
+        local line = vim.fn.line('.')
+        vim.fn.setreg('+', path .. ':' .. line)
+    end,
+    opts
+)
+
+-- unmap the default behavior of `gr` so that it wouldn't clash with the default lsp mappings
+vim.keymap.set('n', 'gr', '', opts)
