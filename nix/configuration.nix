e7e2e4ad6eb13b9ae7bf3345678d2726a54862c7
Egor Bychin <e.siga@proton.me>
2026-03-18 01:56:45 +0400

    nix: fix steam

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -99,6 +99,9 @@
 
   environment.sessionVariables.EDITOR = "nvim";
 
+  # Install steam
+  programs.steam.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -132,7 +135,6 @@
     cargo
 
     osu-lazer-bin
-    steam
 
     #telegram-desktop
     google-chrome
