5a0ee9873dca8be1b3fd1b8fd1147b6200a4299c
Egor Bychin <e.siga@proton.me>
2026-03-31 14:35:49 +0400

    nix: add gnome keyring

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -53,6 +53,8 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
+  services.gnome.gnome-keyring.enable = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us,ru";
