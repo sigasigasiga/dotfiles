fea7f3069898f46dbcef4a38bc595369099973ee
Egor Bychin <e.siga@proton.me>
2026-04-20 14:48:14 +0400

    neovim: move codestyle and keymap settings to `init.lua`

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -93,3 +93,78 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         end
     end,
 })
+
+-- leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local keymap_opts = { noremap = true, silent = true }
+
+-- copy `<filename>:<line_number>`
+vim.keymap.set(
+    'n',
+    '<Leader>yp',
+    function()
+        local path = vim.fn.expand('%:.') -- Get _relative_ path
+        local line = vim.fn.line('.')
+        vim.fn.setreg('+', path .. ':' .. line)
+    end,
+    keymap_opts
+)
+
+-- copy `<filename>:<start_line>-<end_line>` for visual selection
+vim.keymap.set(
+    'v',
+    '<Leader>yp',
+    function()
+        local path = vim.fn.expand('%:.')
+        local start_line = vim.fn.line('v')
+        local end_line = vim.fn.line('.')
+        if start_line > end_line then
+            start_line, end_line = end_line, start_line
+        end
+        vim.fn.setreg('+', path .. ':' .. start_line .. '-' .. end_line)
+    end,
+    keymap_opts
+)
+
+-- unmap the default behavior of `gr` so that it wouldn't clash with the default lsp mappings
+vim.keymap.set('n', 'gr', '', keymap_opts)
+
+-- codestyles
+local codestyles = {
+    drw = {
+        -- tab settings (expandtab changes tabs to spaces)
+        tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true,
+        -- autoindent settings. cinoptions are fucky, so tldr:
+        -- `l1` = don't fuck up curly braces in `case`
+        -- `g0` = access modifiers are not indented
+        -- `N-s` = namespaces are not indented
+        -- `(0,W4` = `void foo(<CR> /* next line is 4 space indented`
+        -- `(s,m1` = `void foo(<CR>` will have `)` on beginning of the next line
+        -- `j1` = don't fuck up lambda definitions in an argument list
+        -- `J1` = don't fuck up JS (and cpp2!) object declarations
+        ai = true, cin = true, cinoptions = 'l1,g0,N-s,(0,W4,(s,m1,j1,J1',
+    },
+
+    gnu = {
+        tabstop = 8, shiftwidth = 2, softtabstop = 2, expandtab = false,
+        ai = true, cin = true, cinoptions='>4,n-2,{2,^-2,:2,=2,g0,h2,p5,t0,+2,(0,u0,w1,m1',
+    }
+}
+
+local set_codestyle = function(style)
+    for k, v in pairs(style) do
+        vim.opt[k] = v
+    end
+end
+
+-- set default codestyle
+set_codestyle(codestyles.drw)
+
+-- set codestyle with ease
+vim.api.nvim_create_user_command(
+    'SetCodestyle',
+    function(params) set_codestyle(codestyles[params.args]) end,
+    { nargs = 1 }
+)
