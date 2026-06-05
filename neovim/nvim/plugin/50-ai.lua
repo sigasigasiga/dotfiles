bc613f7a0746d29d31e444befa5e6dd27a426dc9
Egor Bychin <e.siga@proton.me>
2026-06-05 16:47:38 +0400

    neovim: fix copilot.vim fucking up `vim.snippet.jump`

--- neovim/nvim/plugin/50-ai.lua
+++ neovim/nvim/plugin/50-ai.lua
@@ -1,8 +1,10 @@
+-- This option must be set BEFORE we load the plugin, otherwise `vim.snippet.jump` will not work.
+vim.g.copilot_no_tab_map = true
+
 vim.pack.add {
     'https://github.com/github/copilot.vim',
 }
 
-vim.g.copilot_no_tab_map = true
 vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false
