ca23afd7343eae841e10ee68f37a0be4d1cdee51
Egor Bychin <e.siga@proton.me>
2026-06-05 16:42:44 +0400

    neovim: copilot.vim: remap `<Tab>` to `<C-J>`

--- neovim/nvim/plugin/50-ai.lua
+++ neovim/nvim/plugin/50-ai.lua
@@ -1,3 +1,9 @@
 vim.pack.add {
     'https://github.com/github/copilot.vim',
 }
+
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
