6eae27bd5f1fc80b3a05e6f015518f02a6006d02
Egor Bychin <e.siga@proton.me>
2026-09-04 16:42:24 +0400

    neovim: make branch name in the statusline more visible

--- neovim/nvim/plugin/60-statusline.lua
+++ neovim/nvim/plugin/60-statusline.lua
@@ -1,6 +1,23 @@
-local statusline_git_expr = vim.g.loaded_fugitive and '%{FugitiveStatusline()}' or ''
+function _G.SigaCurrentGitObject()
+    if not vim.g.loaded_fugitive then
+        return ''
+    end
+
+    local fugitive_statusline = vim.fn.FugitiveStatusline()
+
+    if fugitive_statusline == '' then
+        return ''
+    end
+
+    -- The string is of form `[Git<...>]` and we want to remove the brackets and the `Git` prefix
+    local git_object = string.sub(fugitive_statusline, 5, -2):gsub('%%', '%%%%')
+
+    return '%#MiniStatuslineModeVisual#' .. git_object .. '%* '
+end
+
+local statusline_git_expr = '%{%v:lua.SigaCurrentGitObject()%}'
 
 -- it behaves exactly like the default statusline, except:
 -- 1. the visual character number (`%V`) is removed
--- 2. the git status is added (via `FugitiveStatusline()`)
-vim.opt.statusline = '%<%f %h%m%r' .. statusline_git_expr .. '%=%-14.(%l,%c%) %P'
+-- 2. current git object is added
+vim.opt.statusline = '%<' .. statusline_git_expr .. '%f %h%m%r%=%-14.(%l,%c%) %P'
