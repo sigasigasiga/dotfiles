aa4b471842e6503213612cc76b7bc692e6aa4dee
Egor Bychin <e.siga@proton.me>
2026-09-07 16:02:37 +0400

    neovim: add folding support for markdown

---
+++ neovim/nvim/plugin/50-fold.lua
@@ -0,0 +1,9 @@
+-- Enable folding based on treesitter for markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.foldmethod = 'expr'
+    vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.opt_local.foldenable = false
+  end,
+})
